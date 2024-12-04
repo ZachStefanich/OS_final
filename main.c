6306d36139f50db438c3b17882eed1071e47cdc5
unknown <zacharystefanich@gmail.com>
2024-12-03 22:19:48 -0600

    started work on threaded linkedlist, I truly believe this is the faster route to a complete threaded binary tree

--- main.c
+++ main.c
@@ -1,16 +1,38 @@
-#include <pthread.h>
-#include "semaphore.h" 
+#//include <pthread.h>
+//#include "semaphore.h" 
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare semaphores
-sem_t sem1, sem2;
+// Defines the structure of the node type
+typedef struct Node{
+    //data field, this is the value in the node
+    //doesn't have to be a node
+    int data;
+    //pointer to the next node
+    struct Node *next;
+}Node;
 
-
+//function needed to create a new node, and return a pointer to the current node
+Node *createNode(int data){
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 int main() {
-    // Initialize semaphores
-    sem_init(&sem1, 0,0);  // Start at 0, so Thread B waits
-    sem_init(&sem2, 0,0);  // Start at 0, so Thread A waits
-    printf("Test");
+    Node *first = createNode(10);
+    first->next = createNode(20);
+    first->next->next = createNode(30);
+   
+    printf("Linked List: ");
+
+    Node *temp = first;
+    while(temp)
+    {
+        printf("%d", temp->data);
+        printf(" ");
+        temp = temp->next;
+    }
 
     return 0;
 }
